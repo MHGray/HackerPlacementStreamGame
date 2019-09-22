cd8422a64824d131a048e76e658ad3d8b5060bb1
phlip45 <phlip45@Gmail.com>
2019-09-22 00:49:16 -0700

    Getting used to mongoose

---
+++ mongoose/models/hacker.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let Hacker = new Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  active: {
+    type: Boolean,
+    required: true
+  },
+  lastMsg: {
+    type: Date,
+    required: false
+  },
+  credits: {
+    type: Number,
+    required: true
+  },
+  favors: {
+    type: Number,
+    required: true
+  },
+  secrets: {
+    type: Number,
+    required: true
+  },
+  programs: {
+    type: Number,
+    required: true
+  },
+  infamy: {
+    type: Number,
+    required: true
+  },
+  currentNodeName: {
+    type: String,
+    required: false
+  },
+  costumes: {
+    type: Array,
+    required: false
+  },
+  augments: {
+    type: Array,
+    required: false
+  },
+  items: {
+    type: Array,
+    required: false
+  }
+});
+
+module.exports = mongoose.model('Hacker', Hacker);
+/*
+{
+  user: ,
+  image: ,
+  active: ,
+  lastMsg: ,
+  credits: ,
+  favors: ,
+  secrets: ,
+  programs: ,
+  infamy: ,
+  currentNodeName: ,
+  costumers: ,
+  augments: ,
+  items:
+}
+
+
+
+*/
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,26 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const mongoDB = 'mongodb://localhost:27017/twitch';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true }).then(()=> console.log("Mongoose Ready"));
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Hacker = require('./../mongoose/models/hacker.js');
+
+Hacker.findOne({user:"billy"})
+  .then(res=>{
+    if(res == null){
+
+    }else{
+      res.credits += 20;
+      res.save();
+    }    
+  })
+  .catch(err =>{
+    console.log(err);
+  })
 
 
 /* GET home page. */
